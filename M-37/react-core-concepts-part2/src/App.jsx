57a695ce8b1846b53737e04e8eeb10084a6db468
Ranbir <ranbir.webdev@gmail.com>
2025-10-21 23:21:43 +0600

    Added Counter.jsx file

--- M-37/react-core-concepts-part2/src/App.jsx
+++ M-37/react-core-concepts-part2/src/App.jsx
@@ -1,16 +1,44 @@
-
+import Counter from './Counter';
 import './App.css'
 
+// Normal function declaration
 function App() {
+  function handleClick() {
+    alert('Button clicked!')
+  }
 
+  // Arrow function declaration
+  const handleClick3 = () => {
+    alert('Clicked 3')
+  }
+
+  const handleAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  }
   return (
     <>
   
-      <h1>Vite + React</h1>
-     
+      <h3>Vite + React</h3>
+      <Counter></Counter>
+
+
+      {/* normal function */}
+      <button  onClick={handleClick}>Click Me</button>
+      <button  onClick={function handleClick2(){
+        alert('Button 2 clicked!')
+      }}>Click Me</button>
     
+    {/* arrow function */}
+    <button onClick={handleClick3}>Click Me3</button>
+
+    <button onClick={() => alert('Click 4')}>Click 4</button>
+  {/* function with parameter */}
+    <button onClick={() => handleAdd5(7)}>Click Add 5</button>
     </>
   )
 }
+
+
 
 export default App
