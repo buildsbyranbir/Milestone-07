a4e64d5e4df0775e86665ea3416268c104796194
Ranbir <ranbir.webdev@gmail.com>
2025-10-23 17:04:18 +0600

    Added Counter.jsx file

--- M-37/react-core-concepts-part2/src/Counter.jsx
+++ M-37/react-core-concepts-part2/src/Counter.jsx
@@ -1,11 +1,25 @@
+// React theke useState import kora hoyeche
+//karon useState diye component er moddhe state manage kora hoy
+import { useState } from "react"
+
 export default function Counter() {
+    // useState(0) diye ekta state create kora hoyeche
+    // count hocche variable ar setCount hocche tar update korar function
+     const [count, setCount] = useState(0);
+     
+// handleAdd ekta function je count er value 1 kore baray
+     const handleAdd = () => {
+        const newCount = count + 1;
+        setCount(newCount);
+     }
+
     const counterStyle = {
         border: '2px solid yellow',
     }
     return (
         <div style={counterStyle}>
-            <h3>Count:</h3>
-            <button>Add</button>
+            <h3>Count:{count}</h3>
+            <button onClick={handleAdd}>Add</button>
         </div>
     )
 }
