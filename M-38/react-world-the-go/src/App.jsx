53197e334a7de1eb64b369d841ee238aa079cf10
Ranbir <ranbir.webdev@gmail.com>
2025-10-26 01:01:04 +0600

    react-world-on-the-go

---
+++ M-38/react-world-the-go/src/App.jsx
@@ -0,0 +1,13 @@
+import Countries from './components/Countries/Countries'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
